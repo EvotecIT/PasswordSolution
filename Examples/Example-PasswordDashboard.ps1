41cadccf611383c0527606d7986e0b09cb7fe725
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-07-01 09:32:54 +0200

    Sample

---
+++ Examples/Example-PasswordDashboard.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\PasswordSolution.psd1 -Force
+
+$Passwords = Find-Password
+
+New-HTML {
+    New-TableOption -DataStore JavaScript -ArrayJoin -BoolAsString
+    New-HTMLTable -DataTable $Passwords -SearchBuilder -Filtering {
+
+    }
+} -ShowHTML
