3b55505ac0a032976bce2ab8d82c93994e40ce31
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-08-29 21:30:59 +0200

    FInd users data example

---
+++ Examples/Example-FindUsers.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module .\PasswordSolution.psd1 -Force
+
+$Users = Find-Password
+$Users | Format-Table Name, Manager, ManagerSamAccountName, ManagerEmail, ManagerStatus, ManagerLastLogonDays, ManagerType, Domain, UserPrincipalName
