fb975987e02e18e49ed9c4635f20bdb187631019
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-18 10:46:43 +0200

    Update password quality report

--- Public/Show-PasswordQuality.ps1
+++ Public/Show-PasswordQuality.ps1
@@ -1,17 +1,57 @@
 ﻿function Show-PasswordQuality {
+    <#
+    .SYNOPSIS
+    Creates an HTML report showing password quality for all user objects in Active Directory.
+
+    .DESCRIPTION
+    Creates an HTML report showing password quality for all user objects in Active Directory.
+    This comman utilizes DSInternals PowerShell module to get the data.
+    Then it uses PSWriteHTML to create nice looking report.
+
+    .PARAMETER FilePath
+    Path to the file where report will be saved.
+
+    .PARAMETER DontShow
+    If specified, report will not be opened in a browser.
+
+    .PARAMETER Online
+    If specified report will use CDN for JS and CSS files.
+    If not specified, it will merge all CSS and JS files into one HTML file.
+    This makes the file at least 3MB bigger, even if there is very small amount of data.
+    Keep in mindd that this report can be created without internet access, just that using it with -Online switch will require internet access.
+
+    .PARAMETER WeakPasswords
+    List of weak passwords that should be checked for.
+    Provide a list of common passwords that you want to check for, and that your users may have used.
+
+    .PARAMETER SeparateDuplicateGroups
+    If specified, report will show duplicate groups separately, one group per tab.
+
+    .EXAMPLE
+    Show-PasswordQuality -FilePath $PSScriptRoot\Reporting\PasswordQuality.html -Online -WeakPasswords "Test1", "Test2", "Test3" -Verbose
+
+    .EXAMPLE
+    Show-PasswordQuality -FilePath "C:\Support\GitHub\TheDashboard\Ignore\Reports\CustomReports\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html" -WeakPasswords "Test1", "Test2", "Test3" #-Verbose
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         [string] $FilePath,
         [switch] $DontShow,
         [switch] $Online,
-        [string[]] $WeakPasswords
+        [string[]] $WeakPasswords,
+        [switch] $SeparateDuplicateGroups
     )
+    $TimeStart = Start-TimeLog
     $Script:Reporting = [ordered] @{}
     $Script:Reporting['Version'] = Get-GitHubVersion -Cmdlet 'Show-PasswordQuality' -RepositoryOwner 'evotecit' -RepositoryName 'PasswordSolution'
 
 
     Write-Color '[i]', "[PasswordSolution] ", 'Version', ' [Informative] ', $Script:Reporting['Version'] -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
-
+    Write-Color '[i]', ' Gathering passwords dataa ' -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
+    $TimeStartPasswords = Start-TimeLog
     $PasswordQuality = Find-PasswordQuality -IncludeStatistics -WeakPasswords $WeakPasswords
     if (-not $PasswordQuality) {
         # most likely DSInternals not installed
@@ -20,6 +60,11 @@
     $Users = $PasswordQuality.Users
     $Statistics = $PasswordQuality.Statistics
 
+    $EndLogPasswords = Stop-TimeLog -Time $TimeStartPasswords -Option OneLiner
+    Write-Color '[i]', ' Time to gather passwords data ', $EndLogPasswords -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
+
+    $TimeStartHTML = Start-TimeLog
+    Write-Color -Text '[i] ', 'Generating HTML report...' -Color Yellow, DarkGray
     New-HTML {
         New-HTMLTabStyle -BorderRadius 0px -TextTransform capitalize -BackgroundColorActive SlateGrey
         New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
@@ -115,20 +160,92 @@
             'SmartCardUsersWithPassword'
             #'DuplicatePasswordGroups'
         )
+        New-HTMLSection -HeaderText "Password Quality" {
+            New-HTMLTable -DataTable $Users -Filtering {
+                New-HTMLTableCondition -Name 'Enabled' -ComparisonType string -Operator eq -Value $true -BackgroundColor LimeGreen -FailBackgroundColor BlizzardBlue
+                New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator lt -Value 30 -BackgroundColor LimeGreen -HighlightHeaders LastLogonDays, LastLogonDate
+                New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator gt -Value 30 -BackgroundColor Orange -HighlightHeaders LastLogonDays, LastLogonDate
+                New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator gt -Value 60 -BackgroundColor Alizarin -HighlightHeaders LastLogonDays, LastLogonDate
+                New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType string -Operator eq -Value '' -BackgroundColor None -HighlightHeaders LastLogonDays, LastLogonDate
+                New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator ge -Value 0 -BackgroundColor LimeGreen -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 300 -BackgroundColor Orange -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 360 -BackgroundColor Alizarin -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                New-HTMLTableCondition -Name 'PasswordNotRequired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin
+                New-HTMLTableCondition -Name 'PasswordExpired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin -HighlightHeaders PasswordExpired, DaysToExpire, DateExpiry
 
-        New-HTMLTable -DataTable $Users -Filtering {
-            New-HTMLTableCondition -Name 'Enabled' -ComparisonType string -Operator eq -Value $true -BackgroundColor LimeGreen -FailBackgroundColor BlizzardBlue
-            #New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator gt -Value 60 -BackgroundColor Alizarin -HighlightHeaders LastLogonDays, LastLogonDate -FailBackgroundColor LimeGreen
-            New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator ge -Value 0 -BackgroundColor LimeGreen -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
-            New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 300 -BackgroundColor Orange -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
-            New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 360 -BackgroundColor Alizarin -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
-            New-HTMLTableCondition -Name 'PasswordNotRequired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin
-            New-HTMLTableCondition -Name 'PasswordExpired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin -HighlightHeaders PasswordExpired, DaysToExpire, DateExpiry
+                foreach ($Property in $PropertiesHighlight) {
+                    New-HTMLTableCondition -Name $Property -ComparisonType string -Operator eq -Value $true -BackgroundColor Salmon -FailBackgroundColor LightGreen
+                }
+                New-HTMLTableCondition -Name 'DuplicatePasswordGroups' -ComparisonType string -Operator ne -Value "" -BackgroundColor Orange -FailBackgroundColor LightGreen
+            } -ScrollX -ExcludeProperty 'RuleName', 'RuleOptions'
 
-            foreach ($Property in $PropertiesHighlight) {
-                New-HTMLTableCondition -Name $Property -ComparisonType string -Operator eq -Value $true -BackgroundColor Salmon -FailBackgroundColor LightGreen
+        }
+        New-HTMLSection -HeaderText "Duplicate Password Groups" {
+            $TotalDuplicateGroups = 0
+            $EnabledUsersInDuplicateGroups = 0
+            $DisabledUsersInDuplicateGroups = 0
+            $DuplicateGroups = [ordered] @{}
+            foreach ($User in $Users) {
+
+                if ($User.DuplicatePasswordGroups) {
+                    if ($User.Enabled) {
+                        $EnabledUsersInDuplicateGroups++
+                    } else {
+                        $DisabledUsersInDuplicateGroups++
+                    }
+                    if (-not $DuplicateGroups[$User.DuplicatePasswordGroups]) {
+                        $DuplicateGroups[$User.DuplicatePasswordGroups] = [System.Collections.Generic.List[pscustomobject]]::new()
+                    }
+                    $DuplicateGroups[$User.DuplicatePasswordGroups].Add($User)
+                }
             }
-            New-HTMLTableCondition -Name 'DuplicatePasswordGroups' -ComparisonType string -Operator ne -Value "" -BackgroundColor Orange -FailBackgroundColor LightGreen
-        } -ScrollX -ExcludeProperty 'RuleName','RuleOptions'
+            $TotalDuplicateGroups = $DuplicateGroups.Keys.Count
+
+            New-HTMLContainer {
+
+                New-HTMLSection {
+                    New-HTMLPanel {
+                        New-HTMLToast -TextHeader 'Total Duplicate Groups' -Text "Groups of users to review: $TotalDuplicateGroups" -BarColorLeft MayaBlue -IconSolid info-circle -IconColor MayaBlue
+                    } -Invisible
+                    New-HTMLPanel {
+                        New-HTMLToast -TextHeader 'Enabled Users' -Text "Users with duplicate password that are enabled: $EnabledUsersInDuplicateGroups" -BarColorLeft OrangeRed -IconSolid info-circle -IconColor OrangeRed
+                    } -Invisible
+                    New-HTMLPanel {
+                        New-HTMLToast -TextHeader 'Disabled Users' -Text "Users with duplicate password that are disabled: $DisabledUsersInDuplicateGroups" -BarColorLeft OrangePeel -IconSolid info-circle -IconColor OrangePeel
+                    } -Invisible
+                } -Invisible
+
+                New-HTMLSection -Invisible {
+
+                    New-HTMLTabPanel {
+                        foreach ($DuplicateGroup in $DuplicateGroups.Keys) {
+                            New-HTMLTab -Name "$DuplicateGroup ($($DuplicateGroups[$DuplicateGroup].Count))" {
+                                New-HTMLTable -DataTable $DuplicateGroups[$DuplicateGroup] {
+                                    New-HTMLTableCondition -Name 'Enabled' -ComparisonType string -Operator eq -Value $true -BackgroundColor LimeGreen -FailBackgroundColor BlizzardBlue
+                                    New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator lt -Value 30 -BackgroundColor LimeGreen -HighlightHeaders LastLogonDays, LastLogonDate
+                                    New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator gt -Value 30 -BackgroundColor Orange -HighlightHeaders LastLogonDays, LastLogonDate
+                                    New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType number -Operator gt -Value 60 -BackgroundColor Alizarin -HighlightHeaders LastLogonDays, LastLogonDate
+                                    New-HTMLTableCondition -Name 'LastLogonDays' -ComparisonType string -Operator eq -Value '' -BackgroundColor None -HighlightHeaders LastLogonDays, LastLogonDate
+                                    New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator ge -Value 0 -BackgroundColor LimeGreen -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                                    New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 300 -BackgroundColor Orange -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                                    New-HTMLTableCondition -Name 'PasswordLastChangedDays' -ComparisonType number -Operator gt -Value 360 -BackgroundColor Alizarin -HighlightHeaders PasswordLastSet, PasswordLastChangedDays
+                                    New-HTMLTableCondition -Name 'PasswordNotRequired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin
+                                    New-HTMLTableCondition -Name 'PasswordExpired' -ComparisonType string -Operator eq -Value $false -BackgroundColor LimeGreen -FailBackgroundColor Alizarin -HighlightHeaders PasswordExpired, DaysToExpire, DateExpiry
+
+                                    foreach ($Property in $PropertiesHighlight) {
+                                        New-HTMLTableCondition -Name $Property -ComparisonType string -Operator eq -Value $true -BackgroundColor Salmon -FailBackgroundColor LightGreen
+                                    }
+                                } -Filtering -Title "Duplicate Password Group: $DuplicateGroup" -ScrollX -ExcludeProperty 'RuleName', 'RuleOptions'
+                            }
+                        }
+                    }
+                }
+            }
+        }
     } -ShowHTML:(-not $DontShow.IsPresent) -Online:$Online.IsPresent -TitleText "Password Solution - Quality Password Check" -Author "Password Solution" -FilePath $FilePath
+
+    $EndLogHTML = Stop-TimeLog -Time $TimeStartHTML -Option OneLiner
+    $EndLog = Stop-TimeLog -Time $TimeStart -Option OneLiner
+    Write-Color '[i]', 'Time to generate HTML ', $EndLogHTML -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
+    Write-Color '[i]', "[PasswordSolution] ", 'Time to generate ', $EndLog -Color Yellow, DarkGray, Yellow, DarkGray, Magenta
 }
