8224e70f82f275ae8be4109bca0e6c2659a3cf65
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-19 09:22:35 +0200

    Update example

--- Examples/Example-FindUsersPasswordQualityReport.ps1
+++ Examples/Example-FindUsersPasswordQualityReport.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\PasswordSolution.psd1 -Force
 
 #Show-PasswordQuality -FilePath $PSScriptRoot\Reporting\PasswordQuality.html -Online -WeakPasswords "Test1", "Test2", "Test3" -Verbose
-Show-PasswordQuality -FilePath "C:\Support\GitHub\TheDashboard\Ignore\Reports\CustomReports\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html" -WeakPasswords "Test1", "Test2", "Test3" #-Verbose
+Show-PasswordQuality -FilePath "C:\Support\GitHub\TheDashboard\Ignore\Reports\CustomReports\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html" -WeakPasswords "Test1", "Test2", "Test3" -SeparateDuplicateGroups -PassThru
