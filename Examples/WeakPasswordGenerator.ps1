4b9127e564add99d59e74e8ca2baf94d27033cea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-23 14:34:56 +0200

    Generate weak passwords

---
+++ Examples/WeakPasswordGenerator.ps1
@@ -0,0 +1,26 @@
+﻿$Months = @(
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+    "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
+    "Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"
+    "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"
+    "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+) | Sort-Object -Unique
+$Numbers = 0..9
+$Years = 2019..2023
+$SpecialChar = @("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", "|", "\")
+
+$Passwords = foreach ($Year in $Years) {
+    Write-Color -Text "Year: ", $Year -Color Yellow, White
+    $YearPasswords = foreach ($month in $months) {
+        foreach ($number in $numbers) {
+            foreach ($special in $SpecialChar) {
+                $month + $Year.ToString() + $number.ToString() + $special
+                $Year.ToString() + $month + $number.ToString() + $special
+                $month + $Year.ToString() + $special
+            }
+        }
+    }
+    Write-Color -Text "Year: ", $Year, " passwords created: ", $YearPasswords.Count -Color Yellow, White
+    $YearPasswords
+}
+$Passwords.Count
