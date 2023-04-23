82e1cb0f97b007f1e56acd24d0b3436335e3c7ca
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-23 15:34:45 +0200

    Update example

--- Examples/Example-FindUsersPasswordQualityReport.ps1
+++ Examples/Example-FindUsersPasswordQualityReport.ps1
@@ -1,4 +1,15 @@
 ﻿Import-Module .\PasswordSolution.psd1 -Force
 
 #Show-PasswordQuality -FilePath $PSScriptRoot\Reporting\PasswordQuality.html -Online -WeakPasswords "Test1", "Test2", "Test3" -Verbose
-Show-PasswordQuality -FilePath "C:\Support\GitHub\TheDashboard\Ignore\Reports\CustomReports\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html" -WeakPasswords "Test1", "Test2", "Test3" -SeparateDuplicateGroups -PassThru
+$showPasswordQualitySplat = @{
+    #FilePath                = "C:\Support\GitHub\TheDashboard\Ignore\Reports\CustomReports\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html"
+    FilePath                = "$PSScriptRoot\Reporting\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).html"
+    WeakPasswords           = "Test1", "Test2", "Test3", 'February2023!#!@ok', $Passwords | ForEach-Object { $_ }
+    SeparateDuplicateGroups = $true
+    PassThru                = $true
+    AddWorldMap             = $true
+    LogPath                 = "$PSScriptRoot\Logs\PasswordQuality_$(Get-Date -f yyyy-MM-dd_HHmmss).log"
+    Online                  = $true
+}
+
+Show-PasswordQuality @showPasswordQualitySplat
