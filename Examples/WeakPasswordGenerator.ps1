4446a4c206d9bff1b63553a3e5ad1294335e15f5
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-25 09:23:04 +0200

    Update password generator

--- Examples/WeakPasswordGenerator.ps1
+++ Examples/WeakPasswordGenerator.ps1
@@ -1,12 +1,19 @@
 ﻿$Months = @(
+    # english
     "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
-    "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
+    # polish
     "Styczen", "Luty", "Marzec", "Kwiecien", "Maj", "Czerwiec", "Lipiec", "Sierpien", "Wrzesien", "Pazdziernik", "Listopad", "Grudzien"
+    # spanish
+    'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'
+    # german
     "Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"
+    # russian
     "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+    # french
+    'Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'
 ) | Sort-Object -Unique
 $Numbers = 0..9
-$Years = 2019..2023
+$Years = 2020..2023
 $SpecialChar = @("!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "[", "]", "{", "}", "|", "\")
 
 $Passwords = foreach ($Year in $Years) {
