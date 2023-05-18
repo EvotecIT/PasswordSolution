117a4864720e9c7b54afa7c360f8fcd193ba9729
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-18 09:29:56 +0200

    Add defaults function

---
+++ Private/Set-PasswordDefaults.ps1
@@ -0,0 +1,13 @@
+﻿function Set-PasswordDefaults {
+    [CmdletBinding()]
+    param(
+        [System.Collections.IDictionary] $Logging
+    )
+    if (-not $Logging) {
+        $Logging = @{
+            ShowTime   = $true
+            LogFile    = ""
+            TimeFormat = "yyyy-MM-dd HH:mm:ss"
+        }
+    }
+}
