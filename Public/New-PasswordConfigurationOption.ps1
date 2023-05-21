3e69b9ec0d3003d5066f42aba8318afbcb88d247
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-21 09:29:38 +0200

    Build new way for options

---
+++ Public/New-PasswordConfigurationOption.ps1
@@ -0,0 +1,35 @@
+﻿function New-PasswordConfigurationOption {
+    [CmdletBinding()]
+    param(
+        [switch] $ShowTime                     , #= $true
+        [string] $LogFile                      , #= "$PSScriptRoot\Logs\PasswordSolution_$(($Date).ToString('yyyy-MM-dd_HH_mm_ss')).log"
+        [string] $TimeFormat                   , #= "yyyy-MM-dd HH:mm:ss"
+        [int] $LogMaximum                   , #= 365
+        [switch] $NotifyOnSkipUserManagerOnly  , #= $false
+        [switch] $NotifyOnSecuritySend         , #= $true
+        [switch] $NotifyOnManagerSend          , #= $true
+        [switch] $NotifyOnUserSend             , #= $true
+        [switch] $NotifyOnUserMatchingRule     , #= $true
+        [switch] $NotifyOnUserDaysToExpireNull , #= $true
+        [string] $SearchPath
+    )
+
+    $Output = [ordered] @{
+        Type     = "PasswordConfigurationOption"
+        Settings = [ordered] @{
+            ShowTime                     = $ShowTime.IsPresent
+            LogFile                      = $LogFile
+            TimeFormat                   = $TimeFormat
+            LogMaximum                   = $LogMaximum
+            NotifyOnSkipUserManagerOnly  = $NotifyOnSkipUserManagerOnly.IsPresent
+            NotifyOnSecuritySend         = $NotifyOnSecuritySend.IsPresent
+            NotifyOnManagerSend          = $NotifyOnManagerSend.IsPresent
+            NotifyOnUserSend             = $NotifyOnUserSend.IsPresent
+            NotifyOnUserMatchingRule     = $NotifyOnUserMatchingRule.IsPresent
+            NotifyOnUserDaysToExpireNull = $NotifyOnUserDaysToExpireNull.IsPresent
+            SearchPath                   = $SearchPath
+        }
+    }
+    Remove-EmptyValue -Hashtable $Output.Settings
+    $Output
+}
