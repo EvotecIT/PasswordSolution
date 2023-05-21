4fc0107efe9cc38edc0f0c27249d4eaaf1ba0934
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-21 10:05:20 +0200

    Define types

---
+++ Public/New-PasswordConfigurationType.ps1
@@ -0,0 +1,25 @@
+﻿function New-PasswordConfigurationType {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][ValidateSet('User', 'Manager', 'Security', 'Admin')][string] $Type,
+        [switch] $Enable,
+        [int] $SendCountMaximum,
+        #$SendToDefaultEmail,
+        [string] $DefaultEmail,
+        [string] $OverwriteEmailProperty,
+        [switch] $AttachCSV
+    )
+
+    $Output = [ordered] @{
+        Type     = "PasswordConfigurationType$Type"
+        Settings = @{
+            Enable                 = $Enable.IsPresent
+            SendCountMaximum       = $SendCountMaximum
+            #SendToDefaultEmail = $SendToDefaultEmail.IsPresent
+            DefaultEmail           = $DefaultEmail
+            OverwriteEmailProperty = $OverwriteEmailProperty
+            AttachCSV              = $AttachCSV.IsPresent
+        }
+    }
+    $Output
+}
