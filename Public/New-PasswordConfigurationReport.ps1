740f8e1c91ae6cae0ae72ad63ad25637325b4eca
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-21 09:29:45 +0200

    Build new way for report

---
+++ Public/New-PasswordConfigurationReport.ps1
@@ -0,0 +1,48 @@
+﻿function New-PasswordConfigurationReport {
+    [CmdletBinding()]
+    param(
+        [switch] $Enable                , #= $true
+        [switch] $ShowHTML              , #= $true
+        [string] $Title                 , #= "Password Solution Summary"
+        [switch] $Online                , #= $true
+        [switch] $DisableWarnings       , #= $true
+        [switch] $ShowConfiguration     , #= $true
+        [switch] $ShowAllUsers          , #= $true
+        [switch] $ShowRules             , #= $true
+        [switch] $ShowUsersSent         , #= $true
+        [switch] $ShowManagersSent      , #= $true
+        [switch] $ShowEscalationSent    , #= $true
+        [switch] $ShowSkippedUsers      , #= $true
+        [switch] $ShowSkippedLocations  , #= $true
+        [switch] $ShowSearchUsers       , #= $true
+        [switch] $ShowSearchManagers    , #= $true
+        [switch] $ShowSearchEscalations , #= $true
+        [string] $FilePath              , #= "$PSScriptRoot\Reporting\PasswordSolution_$(($Date).ToString('yyyy-MM-dd_HH_mm_ss')).html"
+        [switch] $AttachToEmail           #= $true
+    )
+
+    $Output = [ordered] @{
+        Type     = "PasswordConfigurationReport"
+        Settings = [ordered] @{
+            Enable                = $Enable.IsPresent
+            ShowHTML              = $ShowHTML.IsPresent
+            Title                 = $Title
+            Online                = $Online.IsPresent
+            DisableWarnings       = $DisableWarnings.IsPresent
+            ShowConfiguration     = $ShowConfiguration.IsPresent
+            ShowAllUsers          = $ShowAllUsers.IsPresent
+            ShowRules             = $ShowRules.IsPresent
+            ShowUsersSent         = $ShowUsersSent.IsPresent
+            ShowManagersSent      = $ShowManagersSent.IsPresent
+            ShowEscalationSent    = $ShowEscalationSent.IsPresent
+            ShowSkippedUsers      = $ShowSkippedUsers.IsPresent
+            ShowSkippedLocations  = $ShowSkippedLocations.IsPresent
+            ShowSearchUsers       = $ShowSearchUsers.IsPresent
+            ShowSearchManagers    = $ShowSearchManagers.IsPresent
+            ShowSearchEscalations = $ShowSearchEscalations.IsPresent
+            FilePath              = $FilePath
+            AttachToEmail         = $AttachToEmail.IsPresent
+        }
+    }
+    $Output
+}
