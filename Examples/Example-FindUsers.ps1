2a8f37806a06dc7d8e1ede2dcf6e2ad6ae1cff48
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-25 08:59:29 +0200

    Update example

--- Examples/Example-FindUsers.ps1
+++ Examples/Example-FindUsers.ps1
@@ -1,4 +1,4 @@
 ﻿Import-Module .\PasswordSolution.psd1 -Force
 
-$Users = Find-Password -OverwriteEmailProperty 'extensionAttribute7' #| Where-Object { $_.Name -eq 'Test Contact'}
-$Users | Format-Table Name, Manager, ManagerSamAccountName, ManagerEmail, ManagerStatus, ManagerLastLogonDays, ManagerType, Domain, UserPrincipalName
+$Users = Find-Password -OverwriteEmailProperty 'extensionAttribute7' -OverwriteManagerProperty extensionAttribute1
+$Users | Sort-Object -Property Manager | Format-Table Name, Manager, ManagerSamAccountName, ManagerEmail, ManagerStatus, ManagerLastLogonDays, ManagerType, Domain, UserPrincipalName
