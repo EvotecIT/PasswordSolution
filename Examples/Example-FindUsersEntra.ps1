2b5c7d74699c53613c30881c46a4693eb435b4c1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-27 17:41:06 +0200

    Add basic example

---
+++ Examples/Example-FindUsersEntra.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PasswordSolution.psd1 -Force
+
+
+Connect-MgGraph -Scopes "User.Read.All" -NoWelcome
+
+$Users = Find-PasswordEntra #-Verbose
+#$Users | Format-Table
+$Users | Out-HtmlView -ScrollX -Filtering -DataStore JavaScript {
+    New-HTMLTableCondition -Name 'Enabled' -ComparisonType string -Value $True -BackgroundColor TeaGreen -FailBackgroundColor Salmon
+    New-HTMLTableCondition -Name 'IsLicensed' -ComparisonType string -Value $True -BackgroundColor TeaGreen -FailBackgroundColor Salmon
+    New-HTMLTableCondition -Name 'IsSynchronized' -ComparisonType string -Value $True -BackgroundColor TeaGreen -FailBackgroundColor Salmon
+    New-HTMLTableCondition -Name 'PasswordExpired' -ComparisonType string -Value $false -BackgroundColor TeaGreen -FailBackgroundColor Salmon
+} -DisablePaging
