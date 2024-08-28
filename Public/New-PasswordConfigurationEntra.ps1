b4f68586d801857ca5b52463fd7c5e022118fc07
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-28 11:55:40 +0200

    Add ability to enable Entra

---
+++ Public/New-PasswordConfigurationEntra.ps1
@@ -0,0 +1,13 @@
+﻿function New-PasswordConfigurationEntra {
+    [CmdletBinding()]
+    param(
+        [switch] $Enable
+    )
+    $Output = [ordered] @{
+        Type     = "PasswordConfigurationEntra"
+        Settings = [ordered] @{
+            Enabled = $Enable.IsPresent
+        }
+    }
+    $Output
+}
