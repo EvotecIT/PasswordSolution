64dc593a8a1b9db4fbf4321cdbe8923dd6b4c214
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-09-30 20:16:22 +0200

    Reregister task

---
+++ Examples/Example-ReRegisterTaskAsGMSA.ps1
@@ -0,0 +1,9 @@
+﻿$Tasks = Get-ScheduledTask -TaskPath "\" -TaskName "Automated-PasswordSolution"
+
+# Fix all tasks to use proper account
+foreach ($Task in $Tasks) {
+    schtasks /Change /TN $Task.TaskName /RU "GMSA$" /RP ""
+}
+foreach ($Task in $Tasks) {
+    #Start-ScheduledTask -TaskName $Task.TaskName -Verbose
+}
