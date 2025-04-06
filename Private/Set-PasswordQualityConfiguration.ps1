bb93bd83324feee8c84007908da8398d358ca6ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-04-06 22:14:17 +0200

    feat(Set-PasswordQualityConfiguration): ✨ Add `Set-PasswordQualityConfiguration` function
    * Introduced a new function to configure password quality settings.
    * Supports dynamic configuration through a script block.
    * Collects quality email settings and password configuration email settings.
    * Includes error handling for configuration processing.

---
+++ Private/Set-PasswordQualityConfiguration.ps1
@@ -0,0 +1,26 @@
+﻿function Set-PasswordQualityConfiguration {
+    [CmdletBinding()]
+    param(
+        [scriptblock] $ConfigurationDSL
+    )
+    $OutputInformation = [ordered] @{
+        QualityEmail       = [System.Collections.Generic.List[ordered]]::new()
+        EmailConfiguration = $null
+    }
+    if ($ConfigurationDSL) {
+        try {
+            $ConfigurationExecuted = & $ConfigurationDSL
+            foreach ($Configuration in $ConfigurationExecuted) {
+                if ($Configuration.Type -eq 'QualityEmail') {
+                    $OutputInformation.QualityEmail.Add($Configuration.Settings)
+                } elseif ($Configuration.Type -eq 'PasswordConfigurationEmail') {
+                    $OutputInformation.EmailConfiguration = $Configuration.Settings
+                }
+            }
+        } catch {
+            Write-Color -Text "[e]", " Processing configuration failed because of error in line ", $_.InvocationInfo.ScriptLineNumber, " in ", $_.InvocationInfo.InvocationName, " with message: ", $_.Exception.Message -Color Yellow, White, Red
+            return
+        }
+    }
+    $OutputInformation
+}
